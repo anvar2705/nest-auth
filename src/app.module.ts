25f278bcc571eccfd920406138f0cb7f172922b7
Appazov <anvar978@yandex.ru>
2022-12-13 18:32:58 +0500

    Add envs

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { APP_GUARD } from '@nestjs/core'
 import { ConfigModule } from '@nestjs/config'
 import { TypeOrmModule } from '@nestjs/typeorm'
 
+import { getEnvPath } from 'common/helpers/env.helper'
 import { AuthModule } from './auth/auth.module'
 import { JwtAuthGuard, RolesGuard } from './auth/guards'
 import { UsersModule, RolesModule } from './models'
@@ -11,6 +12,7 @@ import { AppController } from './app.controller'
 @Module({
   imports: [
     ConfigModule.forRoot({
+      envFilePath: getEnvPath(`${__dirname}/common/envs`),
       isGlobal: true,
     }),
     TypeOrmModule.forRoot({
